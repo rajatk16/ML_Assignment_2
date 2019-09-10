cbd9d0b3e31495dac20f25e6345966fe556b6fcd
Rajat Sudagade <rajat.k16@gmail.com>
2019-09-09 21:39:39 -0500

    new changes

--- script3.py
+++ script3.py
@@ -1,35 +1,36 @@
-# Importing the libraries
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+def stock_price_prediction(filePath):
+  `# Importing the libraries
+  import pandas as pd
+  import numpy as np
+  import matplotlib.pyplot as plt
 
-data = pd.read_csv('a.us.csv')
-X = data.iloc[:, 1:-3].values
-y = data.iloc[:, -3].values
+  data = pd.read_csv('a.us.csv')
+  X = data.iloc[:, 1:-3].values
+  y = data.iloc[:, -3].values
 
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X = sc_X.fit_transform(X)
+  from sklearn.preprocessing import StandardScaler
+  sc_X = StandardScaler()
+  X = sc_X.fit_transform(X)
 
-from sklearn.decomposition import PCA
-pca = PCA(n_components=0.99, whiten=True)
-X = pca.fit_transform(X)
+  from sklearn.decomposition import PCA
+  pca = PCA(n_components=0.99, whiten=True)
+  X = pca.fit_transform(X)
 
-plt.scatter(X, y)
-plt.show()
+  plt.scatter(X, y)
+  plt.show()
 
-X_mean = np.mean(X)
-Y_mean = np.mean(y)
+  X_mean = np.mean(X)
+  Y_mean = np.mean(y)
 
-num = 0
-den = 0
-for i in range(len(X)):
-  num += (X[i] - X_mean) * (y[i] - Y_mean)
-  den += (X[i] - X_mean) ** 2
-m = num / den
-c = Y_mean - m*X_mean
+  num = 0
+  den = 0
+  for i in range(len(X)):
+    num += (X[i] - X_mean) * (y[i] - Y_mean)
+    den += (X[i] - X_mean) ** 2
+  m = num / den
+  c = Y_mean - m*X_mean
 
-Y_pred = m*X + c
-plt.scatter(X,y)
-plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color="red")
-plt.show()
+  Y_pred = m*X + c
+  plt.scatter(X,y)
+  plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color="red")
+  plt.show()
