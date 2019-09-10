e13112d4cc4e261aabd155e94493489ef337d783
Rajat Sudagade <rajat.k16@gmail.com>
2019-09-09 21:36:44 -0500

    made changes to OLS code

--- script1.py
+++ script1.py
@@ -1,24 +1,26 @@
+def stock_price_prediction(filePath):
+  import numpy as np
+  import pandas as pd
+  import matplotlib.pyplot as plt
+  import datetime
+  import statsmodels.api as sm
+  from matplotlib.pyplot import style
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import datetime
-import statsmodels.api as sm
-from matplotlib.pyplot import style
 
+  # Set Style for Graphs
+  style.use('ggplot')
 
-# Set Style for Graphs
-style.use('ggplot')
+  # Get Data
+  data = pd.read_csv('a.us.csv')
+  dates = data.index.tolist()
+  prices = data['Close'].tolist()
 
-# Get Data
-data = pd.read_csv('a.us.csv')
-dates = data.index.tolist()
-prices = data['Close'].tolist()
+  dates = sm.add_constant(dates)
 
-dates = sm.add_constant(dates)
+  model = sm.OLS(prices, dates).fit()
+  predictions = model.predict(dates)
 
-model = sm.OLS(prices, dates).fit()
-predictions = model.predict(dates)
+  print_model = model.summary()
+  print(print_model)
 
-print_model = model.summary()
-print(print_model)
+stock_price_prediction('a.us.csv')
